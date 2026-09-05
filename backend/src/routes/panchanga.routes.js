9ae1cbe71ea6ebeb530b5e5fcc9eabeddc21f4b6
shreedhar <shreedharniki@gmail.com>
2026-09-06 01:47:36 +0530

    remove js

--- backend/src/routes/panchanga.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const panchangaController_1 = require("../controllers/panchangaController");
-const router = (0, express_1.Router)();
-router.get("/", panchangaController_1.getPanchang);
-exports.default = router;
