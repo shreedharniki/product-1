aa5a73b0eef3a7c25beb39b8202c1a92ad1a081e
shreedhar <shreedharniki@gmail.com>
2026-09-06 11:11:43 +0530

    remove backend folder

--- backend/src/types/register.ts
+++
@@ -1,9 +0,0 @@
-// src/types/register.ts
-
-export interface RegisterRequest {
-  organization_name: string;
-  temple_name: string;
-  user_name: string;
-  user_email: string;
-  user_phone: string;
-}
--- backend/src/utils/password.ts
+++
@@ -1,6 +0,0 @@
-// src/utils/password.ts
-import crypto from 'crypto';
-
-export function generatePassword(): string {
-  return crypto.randomBytes(12).toString('base64url');
-}
