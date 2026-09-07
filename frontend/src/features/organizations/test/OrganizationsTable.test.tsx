6a1f6a331309fa325d3a55b72dacd564dc6a1694
shreedhar <shreedharniki@gmail.com>
2026-09-07 10:41:51 +0530

    test file outside from the folder and created test folder

--- frontend/src/features/organizations/test/OrganizationsTable.test.tsx
+++ frontend/src/features/organizations/test/OrganizationsTable.test.tsx
@@ -5,7 +5,7 @@ import userEvent from "@testing-library/user-event"
 import { MemoryRouter } from "react-router-dom"
 import "@testing-library/jest-dom"
 
-import OrganizationsTable from "./OrganizationsTable"
+import OrganizationsTable from "@/component/OrganizationsTable"
 
 describe("OrganizationsTable", () => {
   const renderComponent = () => {
