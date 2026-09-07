0fb5be1adef06c4c65e871a31552bf241ce77a92
shreedhar <shreedharniki@gmail.com>
2026-09-07 17:52:36 +0530

    remove org

--- backend/src/features/organizations/services/organizationService.ts
+++
@@ -1,9 +0,0 @@
-import {
-  getOrganizations,
-} from "../repositories/organizationRepository"
-
-export const fetchOrganizations = async () => {
-  const organizations = await getOrganizations()
-
-  return organizations
-}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from "express"
 
-import organizationRoutes from "../features/organizations/routes/organizationRoutes"
+// import organizationRoutes from "../features/organizations/routes/organizationRoutes"
 import moduleRoutes from "../features/modules/routes/moduleRoutes"
 const router = Router()
 
-router.use(organizationRoutes)
+// router.use(organizationRoutes)
 router.use(moduleRoutes)
 export default router
