f7c0f970593476f3dcf1510ce7d4be22351f2607
shreedhar <shreedharniki@gmail.com>
2026-09-09 17:21:46 +0530

    submadule curd backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,8 +2,11 @@ import { Router } from "express"
 
 // import organizationRoutes from "../features/organizations/routes/organizationRoutes"
 import moduleRoutes from "../features/modules/routes/moduleRoutes"
+import subModuleRoutes from "../features/submodule/routes/subModuleRoutes"
+
 const router = Router()
 
 // router.use(organizationRoutes)
 router.use(moduleRoutes)
+router.use("/sub-modules", subModuleRoutes)
 export default router
