8f25c74b1df0c0375c4ed53d5de226e4390e160d
shreedhar <shreedharniki@gmail.com>
2026-09-17 02:04:58 +0530

    docker job

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  //  baseURL: "http://localhost:5001/api",
-   baseURL: import.meta.env.VITE_API_BASE_URL,
+   baseURL: "http://localhost:5001/api",
+  //  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
